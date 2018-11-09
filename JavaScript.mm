ca6f69ff9d705c2b4dd850b7628438d929e9e673
yekingyan <529616@gmail.com>
2018-11-09 17:36:15 +0800

    daily update

--- JavaScript.mm
+++ JavaScript.mm
@@ -166,7 +166,6 @@
     <pre style="background-color: #2b2b2b; color: #a9b7c6; font-family: DejaVu Sans Mono; font-size: 11.3pt">condition ? expr1 : expr2</pre>
   </body>
 </html>
-
 </richcontent>
 </node>
 <node TEXT="condition" ID="ID_1444238098" CREATED="1541642532121" MODIFIED="1541642540711">
@@ -176,6 +175,156 @@
 <node TEXT=" &#x503c;&#x53ef;&#x4ee5;&#x662f;&#x4efb;&#x4f55;&#x7c7b;&#x578b;&#x7684;&#x8868;&#x8fbe;&#x5f0f;" ID="ID_54133376" CREATED="1541642559386" MODIFIED="1541642560473"/>
 </node>
 <node TEXT=" &#x5982;&#x679c;condition&#x4e3a;true&#xff0c;&#x8fd0;&#x7b97;&#x7b26;&#x5c31;&#x4f1a;&#x8fd4;&#x56de; expr1 &#x7684;&#x503c;&#xa;&#x5426;&#x5219;&#xff0c; &#x5c31;&#x4f1a;&#x8fd4;&#x56de; expr2 &#x7684;&#x503c;" ID="ID_645829533" CREATED="1541642570170" MODIFIED="1541642582103"/>
+</node>
+<node TEXT="&#x4f8b;&#x5b50;" ID="ID_397097133" CREATED="1541754571224" MODIFIED="1541754587694">
+<node ID="ID_1515541788" CREATED="1541754661166" MODIFIED="1541754661166"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <pre style="background-color: #2b2b2b; color: #a9b7c6; font-family: DejaVu Sans Mono; font-size: 11.3pt"><span style="color: #cc7832; font-weight: bold"><font color="#cc7832"><b>var </b></font></span>age = <span style="color: #6897bb"><font color="#6897bb">26</font></span><span style="color: #cc7832"><font color="#cc7832">;<br/></font></span><font color="#cc7832"><span style="color: #cc7832; font-weight: bold"><b>var </b></span></font>canDrinkAlcohol = (age &gt; <span style="color: #6897bb"><font color="#6897bb">21</font></span>) ? <span style="color: #6a8759"><font color="#6a8759">&quot;True, over 21&quot; </font></span>: <span style="color: #6a8759"><font color="#6a8759">&quot;False, under 21&quot;</font></span><span style="color: #cc7832"><font color="#cc7832">;<br/></font></span>console.<span style="color: #ffc66d"><font color="#ffc66d">log</font></span>(canDrinkAlcohol)<span style="color: #cc7832"><font color="#cc7832">; </font></span><span style="color: #808080"><font color="#808080">// &quot;True, over 21&quot;</font></span></pre>
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node TEXT=" &#x6839;&#x636e;&#x53d8;&#x91cf;&#x7684;&#x503c;&#x663e;&#x793a;&#x4e0d;&#x540c;&#x7684;&#x4fe1;&#x606f;" ID="ID_926470871" CREATED="1541754679376" MODIFIED="1541754684670">
+<node ID="ID_698943571" CREATED="1541754706482" MODIFIED="1541754706482"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <pre style="background-color: #2b2b2b; color: #a9b7c6; font-family: DejaVu Sans Mono; font-size: 11.3pt"><span style="color: #6a8759"><font color="#6a8759">&quot;The fee is &quot; </font></span>+ (isMember ? <span style="color: #6a8759"><font color="#6a8759">&quot;$2.00&quot; </font></span>: <span style="color: #6a8759"><font color="#6a8759">&quot;$10.00&quot;</font></span>)</pre>
+  </body>
+</html>
+
+</richcontent>
+</node>
+</node>
+<node TEXT="&#x628a;&#x4e09;&#x5143;&#x8fd0;&#x7b97;&#x7b26;&#x7684;&#x503c;&#x8d4b;&#x503c;&#x7ed9;&#x4e00;&#x4e2a;&#x53d8;&#x91cf;" ID="ID_591124488" CREATED="1541754709017" MODIFIED="1541754726335">
+<node ID="ID_329160818" CREATED="1541754748069" MODIFIED="1541754748069"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <pre style="background-color: #2b2b2b; color: #a9b7c6; font-family: DejaVu Sans Mono; font-size: 11.3pt"><span style="color: #cc7832; font-weight: bold"><font color="#cc7832"><b>var </b></font></span>elvisLives = Math.<span style="color: #9876aa"><font color="#9876aa">PI </font></span>&gt; <span style="color: #6897bb"><font color="#6897bb">4 </font></span>? <span style="color: #6a8759"><font color="#6a8759">&quot;Yep&quot; </font></span>: <span style="color: #6a8759"><font color="#6a8759">&quot;Nope&quot;</font></span></pre>
+  </body>
+</html>
+
+</richcontent>
+</node>
+</node>
+<node TEXT="&#x591a;&#x4e2a;&#x4e09;&#x5143;&#x64cd;&#x4f5c;&#x7b26;" ID="ID_738200038" CREATED="1541754878291" MODIFIED="1541754889902">
+<node TEXT="&#x592a;&#x6076;&#x5fc3;&#x4e86;" ID="ID_1864893099" CREATED="1541754965851" MODIFIED="1541754975961"/>
+<node TEXT=" &#x6761;&#x4ef6;&#x8fd0;&#x7b97;&#x7b26;&#x662f;&#x53f3;&#x7ed3;&#x5408;" ID="ID_620045062" CREATED="1541754939435" MODIFIED="1541754940903">
+<node ID="ID_1959780685" CREATED="1541754881855" MODIFIED="1541754881855"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <pre style="background-color: #2b2b2b; color: #a9b7c6; font-family: DejaVu Sans Mono; font-size: 11.3pt"><span style="color: #cc7832; font-weight: bold"><font color="#cc7832"><b>var </b></font></span>firstCheck = <span style="color: #cc7832; font-weight: bold"><font color="#cc7832"><b>false</b></font></span><font color="#cc7832"><span style="color: #cc7832">,<br/></span><span style="color: #cc7832">    </span></font>secondCheck = <span style="color: #cc7832; font-weight: bold"><font color="#cc7832"><b>false</b></font></span><font color="#cc7832"><span style="color: #cc7832">,<br/></span><span style="color: #cc7832">    </span></font>access = firstCheck ? <span style="color: #6a8759"><font color="#6a8759">&quot;Access denied&quot; </font></span>: secondCheck ? <span style="color: #6a8759"><font color="#6a8759">&quot;Access denied&quot; </font></span>: <span style="color: #6a8759"><font color="#6a8759">&quot;Access granted&quot;</font></span><span style="color: #cc7832"><font color="#cc7832">;<br/></font></span><font color="#cc7832"><span style="color: #cc7832">  <br/></span></font>console.<span style="color: #ffc66d"><font color="#ffc66d">log</font></span>( access )<span style="color: #cc7832"><font color="#cc7832">; </font></span><span style="color: #808080"><font color="#808080">// logs &quot;Access granted&quot;</font></span></pre>
+  </body>
+</html>
+
+</richcontent>
+</node>
+</node>
+<node TEXT="&#x50cf;&#x4f7f;&#x7528;&#x591a;&#x91cd;&#x6761;&#x4ef6;&#x7684;IF&#x8868;&#x8fbe;&#x5f0f;&#x4e00;&#x6837;" ID="ID_1175316644" CREATED="1541754944634" MODIFIED="1541754963232">
+<node ID="ID_484295423" CREATED="1541754991884" MODIFIED="1541754991884"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <pre style="background-color: #2b2b2b; color: #a9b7c6; font-family: DejaVu Sans Mono; font-size: 11.3pt"><span style="color: #cc7832; font-weight: bold"><font color="#cc7832"><b>var </b></font></span>condition1 = <span style="color: #cc7832; font-weight: bold"><font color="#cc7832"><b>true</b></font></span><font color="#cc7832"><span style="color: #cc7832">,<br/></span><span style="color: #cc7832">    </span></font>condition2 = <span style="color: #cc7832; font-weight: bold"><font color="#cc7832"><b>false</b></font></span><font color="#cc7832"><span style="color: #cc7832">,<br/></span><span style="color: #cc7832">    </span></font>access = condition1 ? (condition2 ? <span style="color: #6a8759"><font color="#6a8759">&quot;true true&quot;</font></span>: <span style="color: #6a8759"><font color="#6a8759">&quot;true false&quot;</font></span>) : (condition2 ? <span style="color: #6a8759"><font color="#6a8759">&quot;false true&quot; </font></span>: <span style="color: #6a8759"><font color="#6a8759">&quot;false false&quot;</font></span>)<span style="color: #cc7832"><font color="#cc7832">;<br/></font></span><font color="#cc7832"><span style="color: #cc7832"><br/></span></font>console.<span style="color: #ffc66d"><font color="#ffc66d">log</font></span>(access)<span style="color: #cc7832"><font color="#cc7832">; </font></span><span style="color: #808080"><font color="#808080">// </font></span><span style="color: #808080; font-family: AR PL UKai CN"><font color="#808080" face="AR PL UKai CN">&#36755;&#20986;</font></span><span style="color: #808080"><font color="#808080"> &quot;true false&quot;</font></span></pre>
+  </body>
+</html>
+
+</richcontent>
+<node ID="ID_1843436572" CREATED="1541755028790" MODIFIED="1541755028790"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <span http-equiv="content-type" content="text/html; charset=utf-8" style="color: rgb(51, 51, 51); font-family: Arial, x-locale-body, sans-serif; font-size: 16px; font-style: normal; font-weight: 400; letter-spacing: -0.04448px; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px; background-color: rgb(255, 255, 255); display: inline !important; float: none"><font color="rgb(51, 51, 51)" face="Arial, x-locale-body, sans-serif" size="16px">&#19977;&#20803;&#34920;&#36798;&#24335;&#30340;&#25324;&#21495;&#19981;&#26159;&#24517;&#39035;&#30340;&#65292;&#21024;&#21435;&#21518;&#19981;&#24433;&#21709;&#25191;&#34892;&#39034;&#24207;</font></span> 
+  </body>
+</html>
+
+</richcontent>
+</node>
+</node>
+<node TEXT=" &#x4e09;&#x5143;&#x8868;&#x8fbe;&#x5f0f;&#x7684;&#x62ec;&#x53f7;&#x4e0d;&#x662f;&#x5fc5;&#x987b;&#x7684;&#xff0c;&#x5220;&#x53bb;&#x540e;&#x4e0d;&#x5f71;&#x54cd;&#x6267;&#x884c;&#x987a;&#x5e8f;" ID="ID_33105988" CREATED="1541755035882" MODIFIED="1541755037768"/>
+</node>
+</node>
+</node>
+<node TEXT="&#x8fd4;&#x56de;&#x4e09;&#x5143;&#x8bed;&#x53e5;" ID="ID_639056405" CREATED="1541755378063" MODIFIED="1541755384185">
+<node ID="ID_129771532" CREATED="1541755419418" MODIFIED="1541755419418"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <pre style="background-color: #2b2b2b; color: #a9b7c6; font-family: DejaVu Sans Mono; font-size: 11.3pt"><span style="color: #cc7832; font-weight: bold"><font color="#cc7832"><b>var </b></font></span>func1 = <span style="color: #cc7832; font-weight: bold"><font color="#cc7832"><b>function</b></font></span>( .. ) {<br/>  <span style="color: #cc7832; font-weight: bold"><font color="#cc7832"><b>if </b></font></span>(condition1) { <span style="color: #cc7832; font-weight: bold"><font color="#cc7832"><b>return </b></font></span>value1 }<br/>  <span style="color: #cc7832; font-weight: bold"><font color="#cc7832"><b>else </b></font></span>{ <span style="color: #cc7832; font-weight: bold"><font color="#cc7832"><b>return </b></font></span>value2 }<br/>}<br/><br/><span style="color: #cc7832; font-weight: bold"><font color="#cc7832"><b>var </b></font></span>func2 = <span style="color: #cc7832; font-weight: bold"><font color="#cc7832"><b>function</b></font></span>( .. ) {<br/>  <span style="color: #cc7832; font-weight: bold"><font color="#cc7832"><b>return </b></font></span>condition1 ? value1 : value2<br/>}</pre>
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node TEXT="&#x4f8b;&#x5b50;" ID="ID_963873386" CREATED="1541755423652" MODIFIED="1541755432579">
+<node ID="ID_1847578929" CREATED="1541755453354" MODIFIED="1541755453354"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <pre style="background-color: #2b2b2b; color: #a9b7c6; font-family: DejaVu Sans Mono; font-size: 11.3pt"><span style="color: #cc7832; font-weight: bold"><font color="#cc7832"><b>function </b></font></span><span style="color: #ffc66d"><font color="#ffc66d">canDrinkAlcohol</font></span>(age) {<br/>  <span style="color: #cc7832; font-weight: bold"><font color="#cc7832"><b>return </b></font></span>(age &gt; <span style="color: #6897bb"><font color="#6897bb">21</font></span>) ? <span style="color: #6a8759"><font color="#6a8759">&quot;True, over 21&quot; </font></span>: <span style="color: #6a8759"><font color="#6a8759">&quot;False, under 21&quot;</font></span><span style="color: #cc7832"><font color="#cc7832">;<br/></font></span>}<br/><span style="color: #cc7832; font-weight: bold"><font color="#cc7832"><b>var </b></font></span>output = <span style="color: #ffc66d"><font color="#ffc66d">canDrinkAlcohol</font></span>(<span style="color: #6897bb"><font color="#6897bb">26</font></span>)<span style="color: #cc7832"><font color="#cc7832">;<br/></font></span>console.<span style="color: #ffc66d"><font color="#ffc66d">log</font></span>(output)<span style="color: #cc7832"><font color="#cc7832">; </font></span><span style="color: #808080"><font color="#808080">// &quot;True, over 21&quot;</font></span></pre>
+  </body>
+</html>
+
+</richcontent>
+</node>
+</node>
+<node TEXT=" &#x66ff;&#x4ee3;&#x4e00;&#x4e2a;&#x5f88;&#x957f;&#x7684;if/else &#x8868;&#x8fbe;&#x5f0f;" ID="ID_311416825" CREATED="1541755496688" MODIFIED="1541755498557">
+<node ID="ID_1062315316" CREATED="1541755513956" MODIFIED="1541755513956"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <pre style="background-color: #2b2b2b; color: #a9b7c6; font-family: DejaVu Sans Mono; font-size: 11.3pt"><span style="color: #cc7832; font-weight: bold"><font color="#cc7832"><b>var </b></font></span>func1 = <span style="color: #cc7832; font-weight: bold"><font color="#cc7832"><b>function</b></font></span>( .. ) {<br/>  <span style="color: #cc7832; font-weight: bold"><font color="#cc7832"><b>if </b></font></span>(condition1) { <span style="color: #cc7832; font-weight: bold"><font color="#cc7832"><b>return </b></font></span>value1 }<br/>  <span style="color: #cc7832; font-weight: bold"><font color="#cc7832"><b>else if </b></font></span>(condition2) { <span style="color: #cc7832; font-weight: bold"><font color="#cc7832"><b>return </b></font></span>value2 }<br/>  <span style="color: #cc7832; font-weight: bold"><font color="#cc7832"><b>else if </b></font></span>(condition3) { <span style="color: #cc7832; font-weight: bold"><font color="#cc7832"><b>return </b></font></span>value3 }<br/>  <span style="color: #cc7832; font-weight: bold"><font color="#cc7832"><b>else </b></font></span>{ <span style="color: #cc7832; font-weight: bold"><font color="#cc7832"><b>return </b></font></span>value4 }<br/>}</pre>
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node ID="ID_1733591595" CREATED="1541755527408" MODIFIED="1541755527408"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <pre style="background-color: #2b2b2b; color: #a9b7c6; font-family: DejaVu Sans Mono; font-size: 11.3pt"><span style="color: #cc7832; font-weight: bold"><font color="#cc7832"><b>var </b></font></span>func2 = <span style="color: #cc7832; font-weight: bold"><font color="#cc7832"><b>function</b></font></span>( .. ) {<br/>  <span style="color: #cc7832; font-weight: bold"><font color="#cc7832"><b>return </b></font></span>condition1 ? value1<br/>       : condition2 ? value2<br/>       : condition3 ? value3<br/>       :              value4<br/>}</pre>
+  </body>
+</html>
+
+</richcontent>
+</node>
+</node>
 </node>
 </node>
 </node>
