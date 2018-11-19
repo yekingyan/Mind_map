707f60cc987890516c1a7f7d8f008e8069eaf2ed
yekingyan <529616@gmail.com>
2018-11-19 18:15:07 +0800

    daily update

---
+++ freeplane_Model.mm
@@ -0,0 +1,139 @@
+<map version="freeplane 1.6.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="&#x6a21;&#x677f;" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1542618055589" STYLE="bubble">
+<font NAME="Noto Sans S Chinese Light" SIZE="14"/>
+<hook NAME="MapStyle" zoom="1.077">
+    <properties show_icon_for_attributes="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_note_icons="true" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font NAME="Noto Sans S Chinese Light" SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble" VGAP_QUANTITY="6.8999999999999995 pt">
+<font NAME="Noto Sans S Chinese Light" SIZE="12"/>
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="combined" BORDER_WIDTH="3.0 px" MAX_WIDTH="10.1 cm" MIN_WIDTH="0.0 cm" VGAP_QUANTITY="6.8999999999999995 pt">
+<font NAME="Noto Sans S Chinese Light" SIZE="12" BOLD="false" ITALIC="false"/>
+<edge STYLE="sharp_bezier" WIDTH="7"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details" VGAP_QUANTITY="6.8999999999999995 pt">
+<font NAME="Noto Sans S Chinese Light" SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes" VGAP_QUANTITY="6.8999999999999995 pt">
+<font NAME="Noto Sans S Chinese Light" SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT" VGAP_QUANTITY="6.8999999999999995 pt">
+<font NAME="Noto Sans S Chinese Light" SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating" VGAP_QUANTITY="6.8999999999999995 pt">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+<font NAME="Noto Sans S Chinese Light" SIZE="12"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble" VGAP_QUANTITY="6.8999999999999995 pt">
+<font NAME="Noto Sans S Chinese Light" SIZE="12"/>
+<stylenode LOCALIZED_TEXT="styles.ok" VGAP_QUANTITY="6.8999999999999995 pt">
+<icon BUILTIN="button_ok"/>
+<font NAME="Noto Sans S Chinese Light" SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.needs_action" VGAP_QUANTITY="6.8999999999999995 pt">
+<icon BUILTIN="messagebox_warning"/>
+<font NAME="Noto Sans S Chinese Light" SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.floating_node" VGAP_QUANTITY="6.8999999999999995 pt">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+<font NAME="Noto Sans S Chinese Light" SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork" VGAP_QUANTITY="6.8999999999999995 pt">
+<font NAME="Noto Sans S Chinese Light" SIZE="12" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork" VGAP_QUANTITY="6.8999999999999995 pt">
+<font NAME="Noto Sans S Chinese Light" SIZE="12" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900" VGAP_QUANTITY="6.8999999999999995 pt">
+<font NAME="Noto Sans S Chinese Light" SIZE="12" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.connection" COLOR="#606060" STYLE="fork" VGAP_QUANTITY="6.8999999999999995 pt">
+<font NAME="Noto Sans S Chinese Light" SIZE="12" BOLD="false"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important" COLOR="#ff0000" VGAP_QUANTITY="6.8999999999999995 pt">
+<icon BUILTIN="yes"/>
+<font NAME="Noto Sans S Chinese Light" SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.question" VGAP_QUANTITY="6.8999999999999995 pt">
+<icon BUILTIN="help"/>
+<font NAME="Noto Sans S Chinese Light" SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.key" COLOR="#996600" VGAP_QUANTITY="6.8999999999999995 pt">
+<icon BUILTIN="password"/>
+<font NAME="Noto Sans S Chinese Light" SIZE="12" BOLD="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.idea" VGAP_QUANTITY="6.8999999999999995 pt">
+<icon BUILTIN="idea"/>
+<font NAME="Noto Sans S Chinese Light" SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.note" COLOR="#990000" VGAP_QUANTITY="6.8999999999999995 pt">
+<font NAME="Noto Sans S Chinese Light" SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.date" COLOR="#0033ff" VGAP_QUANTITY="6.8999999999999995 pt">
+<icon BUILTIN="calendar"/>
+<font NAME="Noto Sans S Chinese Light" SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.website" COLOR="#006633" VGAP_QUANTITY="6.8999999999999995 pt">
+<font NAME="Noto Sans S Chinese Light" SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.list" COLOR="#cc6600" VGAP_QUANTITY="6.8999999999999995 pt">
+<icon BUILTIN="list"/>
+<font NAME="Noto Sans S Chinese Light" SIZE="12" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.quotation" COLOR="#338800" STYLE="fork" VGAP_QUANTITY="6.8999999999999995 pt">
+<font NAME="Noto Sans S Chinese Light" SIZE="12" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.definition" COLOR="#666600" VGAP_QUANTITY="6.8999999999999995 pt">
+<font NAME="Noto Sans S Chinese Light" SIZE="12" BOLD="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.description" COLOR="#996600" VGAP_QUANTITY="6.8999999999999995 pt">
+<font NAME="Noto Sans S Chinese Light" SIZE="12" BOLD="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.pending" COLOR="#b3b95c" VGAP_QUANTITY="6.8999999999999995 pt">
+<font NAME="Noto Sans S Chinese Light" SIZE="12"/>
+</stylenode>
+<stylenode TEXT="&#x4ee3;&#x7801;" COLOR="#c0fdce" BACKGROUND_COLOR="#000000" VGAP_QUANTITY="6.8999999999999995 pt">
+<font NAME="Noto Sans S Chinese Light" SIZE="12"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble" VGAP_QUANTITY="6.8999999999999995 pt">
+<font NAME="Noto Sans S Chinese Light" SIZE="12"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" VGAP_QUANTITY="6.8999999999999995 pt">
+<font NAME="Noto Sans S Chinese Light" SIZE="12"/>
+<edge COLOR="#003333"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff" VGAP_QUANTITY="6.8999999999999995 pt">
+<font NAME="Noto Sans S Chinese Light" SIZE="12"/>
+<edge COLOR="#808080"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#003366" VGAP_QUANTITY="6.8999999999999995 pt">
+<font NAME="Noto Sans S Chinese Light" SIZE="12"/>
+<edge COLOR="#808080"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000" VGAP_QUANTITY="6.8999999999999995 pt">
+<font NAME="Noto Sans S Chinese Light" SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111" VGAP_QUANTITY="6.8999999999999995 pt">
+<font NAME="Noto Sans S Chinese Light" SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5" COLOR="#006666" VGAP_QUANTITY="6.8999999999999995 pt">
+<font NAME="Noto Sans S Chinese Light" SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6" VGAP_QUANTITY="6.8999999999999995 pt">
+<font SIZE="12"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="96" RULE="ON_BRANCH_CREATION"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+</node>
+</map>
