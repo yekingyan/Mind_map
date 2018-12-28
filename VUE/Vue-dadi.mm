1e6a10a410a8817b5d73410c5a00f1841437c6e5
yekingyan <529616@gmail.com>
2018-12-29 00:05:02 +0800

    daily update

--- VUE/Vue-dadi.mm
+++ VUE/Vue-dadi.mm
@@ -3,7 +3,7 @@
 <node TEXT="Vue-dadi" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1545923075600" STYLE="bubble">
 <font NAME="Noto Sans S Chinese Light" SIZE="14"/>
 <hook NAME="MapStyle" zoom="1.077">
-    <properties show_icon_for_attributes="true" fit_to_viewport="false" show_note_icons="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff"/>
+    <properties fit_to_viewport="false" show_icon_for_attributes="true" show_note_icons="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff"/>
 
 <map_styles>
 <stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
@@ -133,7 +133,64 @@
 </stylenode>
 </map_styles>
 </hook>
-<hook NAME="AutomaticEdgeColor" COUNTER="90" RULE="ON_BRANCH_CREATION"/>
+<hook NAME="AutomaticEdgeColor" COUNTER="94" RULE="ON_BRANCH_CREATION"/>
 <hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<node TEXT="&#x521d;&#x59cb;&#x5316;" POSITION="right" ID="ID_1107097771" CREATED="1545923177136" MODIFIED="1545923180562">
+<edge COLOR="#00ffff"/>
+<node TEXT="&#x73af;&#x5883;" ID="ID_1936770946" CREATED="1545923118224" MODIFIED="1545923183538">
+<node TEXT="node.js" ID="ID_1933985657" CREATED="1545923124928" MODIFIED="1545923127906"/>
+<node TEXT="vue-cli &#x811a;&#x624b;&#x67b6;" ID="ID_874106822" CREATED="1545923131776" MODIFIED="1545923156698">
+<node TEXT="npm install --global vue-cli" ID="ID_883539954" CREATED="1546010210471" MODIFIED="1546010228438"/>
+<node TEXT="&#x6216;&#x8005;&#x7528;cnpm" ID="ID_1924775200" CREATED="1546010230878" MODIFIED="1546010244041"/>
+</node>
+</node>
+<node TEXT="&#x5305;&#x7ba1;&#x7406;&#x5de5;&#x5177;" ID="ID_1317709603" CREATED="1546010585376" MODIFIED="1546010598906">
+<node TEXT="npm" ID="ID_34088337" CREATED="1546010600879" MODIFIED="1546010604001">
+<node TEXT="&#x88c5;&#x5b8c;node.js &#x5c31;&#x6709;&#x4e86;" ID="ID_1092889164" CREATED="1546011180168" MODIFIED="1546011187258"/>
+</node>
+<node TEXT="cnpm" ID="ID_1879044722" CREATED="1546010604263" MODIFIED="1546010608545">
+<node TEXT="&#x6dd8;&#x5b9d;&#x7684;&#x670d;&#x52a1;&#x5668;" ID="ID_1698796679" CREATED="1546011024936" MODIFIED="1546011046690"/>
+<node TEXT="&#x5feb;&#x4e00;&#x70b9;" ID="ID_1601418853" CREATED="1546011102320" MODIFIED="1546011104865"/>
+</node>
+<node TEXT="yarn" ID="ID_1155549970" CREATED="1546010608927" MODIFIED="1546010610825"/>
+</node>
+<node TEXT="&#x521b;&#x5efa;&#x9879;&#x76ee;" ID="ID_148296257" CREATED="1545923169216" MODIFIED="1545923182180">
+<node TEXT="&#x5fc5;&#x987b;&#x5728;&#x5bf9;&#x5e94;&#x7684;&#x9879;&#x76ee;&#x76ee;&#x5f55;&#x4e0b;&#x8fd0;&#x884c;&#x547d;&#x4ee4;" ID="ID_688584446" CREATED="1546010202133" MODIFIED="1546010297305"/>
+<node TEXT="vue init webpack vue-demo01" ID="ID_657585370" CREATED="1546010897271" MODIFIED="1546010914273">
+<node TEXT="&#x7528;&#x6cd5;&#xff1a; vue init &lt;template-name&gt; &lt;project-name&gt;" ID="ID_1287564036" CREATED="1546012688705" MODIFIED="1546012695779"/>
+<node TEXT="template-name" ID="ID_1948406837" CREATED="1546012708738" MODIFIED="1546012722171">
+<node TEXT=" webpack" ID="ID_1209353423" CREATED="1546012722809" MODIFIED="1546012732819"/>
+<node TEXT="webpack-simple" ID="ID_1994973080" CREATED="1546012733705" MODIFIED="1546012786851">
+<node TEXT=" &#x4e00;&#x4e2a;&#x7b80;&#x5355;webpack+vue-loader&#x7684;&#x6a21;&#x677f;&#xff0c;&#x4e0d;&#x5305;&#x542b;&#x5176;&#x4ed6;&#x529f;&#x80fd;" ID="ID_1638848170" CREATED="1546012788366" MODIFIED="1546012792779"/>
+</node>
+<node TEXT="browserify" ID="ID_182705862" CREATED="1546012742537" MODIFIED="1546012797843">
+<node TEXT="&#x4e00;&#x4e2a;&#x5168;&#x9762;&#x7684;Browserify+vueify &#x7684;&#x6a21;&#x677f;&#xff0c;&#x529f;&#x80fd;&#x5305;&#x62ec;&#x70ed;&#x52a0;&#x8f7d;&#xff0c;linting,&#x5355;&#x5143;&#x68c0;&#x6d4b;" ID="ID_817834196" CREATED="1546012798820" MODIFIED="1546012802619"/>
+</node>
+<node TEXT="browserify-simple" ID="ID_177077303" CREATED="1546012747897" MODIFIED="1546012806234">
+<node TEXT="&#x4e00;&#x4e2a;&#x7b80;&#x5355;Browserify+vueify&#x7684;&#x6a21;&#x677f;&#xff0c;&#x4e0d;&#x5305;&#x542b;&#x5176;&#x4ed6;&#x529f;&#x80fd;" ID="ID_1025225394" CREATED="1546012806988" MODIFIED="1546012812187"/>
+</node>
+<node TEXT="pwa" ID="ID_760621160" CREATED="1546012755025" MODIFIED="1546012817115">
+<node TEXT="&#x57fa;&#x4e8e;webpack&#x6a21;&#x677f;&#x7684;vue-cli&#x7684;PWA&#x6a21;&#x677f;" ID="ID_1078929390" CREATED="1546012817898" MODIFIED="1546012820547"/>
+</node>
+<node TEXT="simple" ID="ID_1205568901" CREATED="1546012761713" MODIFIED="1546012825555">
+<node TEXT="&#x4e00;&#x4e2a;&#x6700;&#x7b80;&#x5355;&#x7684;&#x5355;&#x9875;&#x5e94;&#x7528;&#x6a21;&#x677f;" ID="ID_1766250052" CREATED="1546012826283" MODIFIED="1546012829731"/>
+</node>
+</node>
+<node TEXT=" &#x9879;&#x76ee;&#x7684;&#x540d;&#x79f0;&#x4e0d;&#x80fd;&#x5927;&#x5199;&#xff0c;&#x4e0d;&#x7136;&#x4f1a;&#x62a5;&#x9519;" ID="ID_1072152792" CREATED="1546012886834" MODIFIED="1546012896059"/>
+</node>
+<node TEXT="cd vue-demo01" ID="ID_1986962539" CREATED="1546011672080" MODIFIED="1546011680649"/>
+<node TEXT="npm run dev" ID="ID_823755052" CREATED="1546011680880" MODIFIED="1546011689321"/>
+</node>
+<node TEXT="git &#x56de;&#x6765;&#x7684;&#x4ee3;&#x7801;&#x88c5;&#x73af;&#x5883;" ID="ID_996640667" CREATED="1546012490329" MODIFIED="1546012503875">
+<node TEXT="&#x9879;&#x76ee;&#x9ed8;&#x8ba4; git ignore node_modules" ID="ID_295781054" CREATED="1546012391497" MODIFIED="1546012478395"/>
+<node TEXT="&#x5728;&#x9879;&#x76ee;&#x76ee;&#x5f55;&#x4e0b;" ID="ID_589735511" CREATED="1546012507433" MODIFIED="1546012523763">
+<node TEXT="npm install" ID="ID_1069997892" CREATED="1546012523993" MODIFIED="1546012529130"/>
+<node TEXT="&#x627e;&#x5230;package.json&#x7684;&#x5305;&#x5b89;&#x88c5;" ID="ID_658581269" CREATED="1546012535961" MODIFIED="1546012559530"/>
+</node>
+</node>
+</node>
+<node TEXT="vue-cli&#x9879;&#x76ee;&#x7ed3;&#x6784;" POSITION="right" ID="ID_1581444271" CREATED="1546012604890" MODIFIED="1546012617099">
+<edge COLOR="#7c0000"/>
+</node>
 </node>
 </map>
