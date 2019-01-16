964e1b92aadb4cc6560ef00bd1451dcd8a08e16f
yekingyan <529616@gmail.com>
2019-01-17 00:50:27 +0800

    python2.7xadmin

---
+++ Django/python2.7xadmin/django_xadmin.mm
@@ -0,0 +1,224 @@
+<map version="freeplane 1.6.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="django" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1547645164509" STYLE="bubble">
+<font NAME="Noto Sans S Chinese Light" SIZE="14"/>
+<hook NAME="MapStyle" zoom="1.077">
+    <properties fit_to_viewport="false" show_icon_for_attributes="true" show_note_icons="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font NAME="Noto Sans S Chinese Light" SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble" VGAP_QUANTITY="6.8999999999999995 pt">
+<font NAME="Noto Sans S Chinese Light" SIZE="12"/>
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="combined" BORDER_WIDTH="3.0 px" MAX_WIDTH="10.1 cm" MIN_WIDTH="0.0 cm" VGAP_QUANTITY="6.8999999999999995 pt">
+<font NAME="Noto Sans S Chinese Light" SIZE="12" BOLD="false" ITALIC="false"/>
+<edge STYLE="sharp_bezier" WIDTH="7"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details" VGAP_QUANTITY="6.8999999999999995 pt">
+<font NAME="Noto Sans S Chinese Light" SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes" VGAP_QUANTITY="6.8999999999999995 pt">
+<font NAME="Noto Sans S Chinese Light" SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT" VGAP_QUANTITY="6.8999999999999995 pt">
+<font NAME="Noto Sans S Chinese Light" SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating" VGAP_QUANTITY="6.8999999999999995 pt">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+<font NAME="Noto Sans S Chinese Light" SIZE="12"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble" VGAP_QUANTITY="6.8999999999999995 pt">
+<font NAME="Noto Sans S Chinese Light" SIZE="12"/>
+<stylenode LOCALIZED_TEXT="styles.ok" VGAP_QUANTITY="6.8999999999999995 pt">
+<icon BUILTIN="button_ok"/>
+<font NAME="Noto Sans S Chinese Light" SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.needs_action" VGAP_QUANTITY="6.8999999999999995 pt">
+<icon BUILTIN="messagebox_warning"/>
+<font NAME="Noto Sans S Chinese Light" SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.floating_node" VGAP_QUANTITY="6.8999999999999995 pt">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+<font NAME="Noto Sans S Chinese Light" SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork" VGAP_QUANTITY="6.8999999999999995 pt">
+<font NAME="Noto Sans S Chinese Light" SIZE="12" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork" VGAP_QUANTITY="6.8999999999999995 pt">
+<font NAME="Noto Sans S Chinese Light" SIZE="12" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900" VGAP_QUANTITY="6.8999999999999995 pt">
+<font NAME="Noto Sans S Chinese Light" SIZE="12" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.connection" COLOR="#606060" STYLE="fork" VGAP_QUANTITY="6.8999999999999995 pt">
+<font NAME="Noto Sans S Chinese Light" SIZE="12" BOLD="false"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important" COLOR="#ff0000" VGAP_QUANTITY="6.8999999999999995 pt">
+<icon BUILTIN="yes"/>
+<font NAME="Noto Sans S Chinese Light" SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.question" VGAP_QUANTITY="6.8999999999999995 pt">
+<icon BUILTIN="help"/>
+<font NAME="Noto Sans S Chinese Light" SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.key" COLOR="#996600" VGAP_QUANTITY="6.8999999999999995 pt">
+<icon BUILTIN="password"/>
+<font NAME="Noto Sans S Chinese Light" SIZE="12" BOLD="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.idea" VGAP_QUANTITY="6.8999999999999995 pt">
+<icon BUILTIN="idea"/>
+<font NAME="Noto Sans S Chinese Light" SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.note" COLOR="#990000" VGAP_QUANTITY="6.8999999999999995 pt">
+<font NAME="Noto Sans S Chinese Light" SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.date" COLOR="#0033ff" VGAP_QUANTITY="6.8999999999999995 pt">
+<icon BUILTIN="calendar"/>
+<font NAME="Noto Sans S Chinese Light" SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.website" COLOR="#006633" VGAP_QUANTITY="6.8999999999999995 pt">
+<font NAME="Noto Sans S Chinese Light" SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.list" COLOR="#cc6600" VGAP_QUANTITY="6.8999999999999995 pt">
+<icon BUILTIN="list"/>
+<font NAME="Noto Sans S Chinese Light" SIZE="12" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.quotation" COLOR="#338800" STYLE="fork" VGAP_QUANTITY="6.8999999999999995 pt">
+<font NAME="Noto Sans S Chinese Light" SIZE="12" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.definition" COLOR="#666600" VGAP_QUANTITY="6.8999999999999995 pt">
+<font NAME="Noto Sans S Chinese Light" SIZE="12" BOLD="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.description" COLOR="#996600" VGAP_QUANTITY="6.8999999999999995 pt">
+<font NAME="Noto Sans S Chinese Light" SIZE="12" BOLD="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.pending" COLOR="#b3b95c" VGAP_QUANTITY="6.8999999999999995 pt">
+<font NAME="Noto Sans S Chinese Light" SIZE="12"/>
+</stylenode>
+<stylenode TEXT="&#x4ee3;&#x7801;" COLOR="#c0fdce" BACKGROUND_COLOR="#000000" VGAP_QUANTITY="6.8999999999999995 pt">
+<font NAME="Noto Sans S Chinese Light" SIZE="12"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble" VGAP_QUANTITY="6.8999999999999995 pt">
+<font NAME="Noto Sans S Chinese Light" SIZE="12"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" VGAP_QUANTITY="6.8999999999999995 pt">
+<font NAME="Noto Sans S Chinese Light" SIZE="12"/>
+<edge COLOR="#003333"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff" VGAP_QUANTITY="6.8999999999999995 pt">
+<font NAME="Noto Sans S Chinese Light" SIZE="12"/>
+<edge COLOR="#808080"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#003366" VGAP_QUANTITY="6.8999999999999995 pt">
+<font NAME="Noto Sans S Chinese Light" SIZE="12"/>
+<edge COLOR="#808080"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000" VGAP_QUANTITY="6.8999999999999995 pt">
+<font NAME="Noto Sans S Chinese Light" SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111" VGAP_QUANTITY="6.8999999999999995 pt">
+<font NAME="Noto Sans S Chinese Light" SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5" COLOR="#006666" VGAP_QUANTITY="6.8999999999999995 pt">
+<font NAME="Noto Sans S Chinese Light" SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6" VGAP_QUANTITY="6.8999999999999995 pt">
+<font SIZE="12"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="99" RULE="ON_BRANCH_CREATION"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<node TEXT="virtualenv" POSITION="right" ID="ID_1420307328" CREATED="1547647535622" MODIFIED="1547647583481" VGAP_QUANTITY="6.8999999999999995 pt">
+<edge COLOR="#7c007c"/>
+<node TEXT="&#x5b89;&#x88c5;" ID="ID_888232658" CREATED="1547647594124" MODIFIED="1547647595543">
+<node TEXT="pip install virtualenv" ID="ID_1695248027" CREATED="1547647544653" MODIFIED="1547647592993"/>
+</node>
+<node TEXT="&#x65b0;&#x5efa;" ID="ID_1058586338" CREATED="1547647600693" MODIFIED="1547647613695">
+<node TEXT="virtualenv demo" ID="ID_1046232302" CREATED="1547647613965" MODIFIED="1547647624831"/>
+</node>
+<node TEXT="&#x6fc0;&#x6d3b;" ID="ID_334042642" CREATED="1547647628414" MODIFIED="1547647633830">
+<node TEXT="$ cd \venv\Scripts\" ID="ID_1834163218" CREATED="1547647634212" MODIFIED="1547647674798"/>
+<node TEXT="$ activate" ID="ID_1617161480" CREATED="1547647648708" MODIFIED="1547647679414"/>
+</node>
+<node TEXT="&#x9000;&#x51fa;" ID="ID_592877645" CREATED="1547647683117" MODIFIED="1547647688824">
+<node TEXT="deactivate" ID="ID_229076969" CREATED="1547647689300" MODIFIED="1547647702958"/>
+</node>
+</node>
+<node TEXT="MySQL" POSITION="right" ID="ID_1687147544" CREATED="1547653540785" MODIFIED="1547653933800">
+<edge COLOR="#007c7c"/>
+<node TEXT="navicat for mySQL &#x8fde;&#x63a5;&#x6570;&#x636e;&#x5e93;" ID="ID_512870516" CREATED="1547653543904" MODIFIED="1547653599227"/>
+<node TEXT="&#x5b89;&#x88c5;&#x9a71;&#x52a8;" ID="ID_754504913" CREATED="1547653568889" MODIFIED="1547654919584">
+<node TEXT=" pip install mysql-python" ID="ID_1014403154" CREATED="1547653608272" MODIFIED="1547653666962"/>
+<node TEXT="&#x4e0a;&#x4e00;&#x6761;&#x5931;&#x8d25;&#x5219;&#x4e0b;&#x8f7d;.whl&#x5305;" ID="ID_1148328862" CREATED="1547653637464" MODIFIED="1547654939136">
+<node TEXT=" https://www.lfd.uci.edu/~gohlke/pythonlibs/#mysql-python" ID="ID_668272962" CREATED="1547654940511" MODIFIED="1547654941639"/>
+<node TEXT="pip install MySQL_python-1.2.5-cp27-none-win_amd64.whl" ID="ID_1831033766" CREATED="1547654944935" MODIFIED="1547655063982"/>
+</node>
+</node>
+<node TEXT="pycharm&#x4e2d;" ID="ID_872667501" CREATED="1547655097092" MODIFIED="1547655101350">
+<node TEXT="&#x8fdb;&#x5165;shell" ID="ID_618287848" CREATED="1547655886044" MODIFIED="1547655895237">
+<node TEXT="Tools -&gt; run manage.py Task" ID="ID_1513006726" CREATED="1547655101613" MODIFIED="1547655134055"/>
+</node>
+<node TEXT="&#x751f;&#x6210;&#x8868;" ID="ID_111915754" CREATED="1547655742236" MODIFIED="1547655751486">
+<node TEXT="python manage.py makemigrations&#xa;python manage.py migrate" ID="ID_1994148712" CREATED="1547655752220" MODIFIED="1547655882549"/>
+</node>
+<node TEXT="&#x8868;&#x751f;&#x6210;&#x5c31;&#x53ef;&#x4ee5;&#x8fdb;&#x884c;django&#x4e86;" ID="ID_761133679" CREATED="1547655902180" MODIFIED="1547655914533"/>
+</node>
+</node>
+<node TEXT="settings.py" POSITION="right" ID="ID_1478713378" CREATED="1547656849539" MODIFIED="1547656855892">
+<edge COLOR="#7c7c00"/>
+<node TEXT="&#x914d;&#x7f6e;template" ID="ID_1650928381" CREATED="1547656856226" MODIFIED="1547656864547">
+<node ID="ID_1777740533" CREATED="1547656878053" MODIFIED="1547656878053"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <pre style="background-color: #2b2b2b; color: #a9b7c6; font-family: &#x5b8b;&#x4f53;; font-size: 10.5pt"><span style="color: #a5c261"><font color="#a5c261">'DIRS'</font></span>: [os.path.join(BASE_DIR<span style="color: #cc7832"><font color="#cc7832">, </font></span><span style="color: #a5c261"><font color="#a5c261">'templates'</font></span>)]<span style="color: #cc7832"><font color="#cc7832">,</font></span></pre>
+  </body>
+</html>
+
+</richcontent>
+</node>
+</node>
+<node TEXT="&#x914d;&#x7f6e;static" ID="ID_250382219" CREATED="1547656881161" MODIFIED="1547656887626">
+<node ID="ID_1661615708" CREATED="1547656902647" MODIFIED="1547656902647"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <pre style="background-color: #2b2b2b; color: #a9b7c6; font-family: &#x5b8b;&#x4f53;; font-size: 10.5pt">STATIC_URL = <span style="color: #a5c261"><font color="#a5c261">'/static/'<br/></font></span>STATICFILES_DIRS = [<br/>    os.path.join(BASE_DIR<span style="color: #cc7832"><font color="#cc7832">, </font></span><span style="color: #a5c261"><font color="#a5c261">'static'</font></span>)<span style="color: #cc7832"><font color="#cc7832">,<br/></font></span>]</pre>
+  </body>
+</html>
+
+</richcontent>
+</node>
+</node>
+<node TEXT="&#x914d;&#x7f6e; app" ID="ID_1557611872" CREATED="1547656946618" MODIFIED="1547656954652"/>
+<node TEXT="&#x914d;&#x7f6e;&#x6570;&#x636e;&#x5e93;" ID="ID_1554546716" CREATED="1547656971505" MODIFIED="1547656976595">
+<node ID="ID_1231558415" CREATED="1547656977354" MODIFIED="1547656977354"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <pre style="background-color: #2b2b2b; color: #a9b7c6; font-family: &#x5b8b;&#x4f53;; font-size: 10.5pt">DATABASES = {<br/>    <span style="color: #a5c261"><font color="#a5c261">'default'</font></span>: {<br/>        <span style="color: #a5c261"><font color="#a5c261">'ENGINE'</font></span>: <span style="color: #a5c261"><font color="#a5c261">'django.db.backends.mysql'</font></span><span style="color: #cc7832"><font color="#cc7832">,<br/></font></span><font color="#cc7832"><span style="color: #cc7832">        </span></font><span style="color: #a5c261"><font color="#a5c261">'NAME'</font></span>: <span style="color: #a5c261"><font color="#a5c261">'testdjango'</font></span><span style="color: #cc7832"><font color="#cc7832">,<br/></font></span><font color="#cc7832"><span style="color: #cc7832">        </span></font><span style="color: #a5c261"><font color="#a5c261">'USER'</font></span>: <span style="color: #a5c261"><font color="#a5c261">'root'</font></span><span style="color: #cc7832"><font color="#cc7832">,<br/></font></span><font color="#cc7832"><span style="color: #cc7832">        </span></font><span style="color: #a5c261"><font color="#a5c261">'PASSWORD'</font></span>: <span style="color: #a5c261"><font color="#a5c261">'12345678'</font></span><span style="color: #cc7832"><font color="#cc7832">,<br/></font></span><font color="#cc7832"><span style="color: #cc7832">        </span></font><span style="color: #a5c261"><font color="#a5c261">'HOST'</font></span>: <span style="color: #a5c261"><font color="#a5c261">'127.0.0.1'</font></span><span style="color: #cc7832"><font color="#cc7832">,<br/></font></span><font color="#cc7832"><span style="color: #cc7832">    </span></font>}<br/>}</pre>
+  </body>
+</html>
+
+</richcontent>
+</node>
+</node>
+</node>
+</node>
+</map>
