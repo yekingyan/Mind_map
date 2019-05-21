16ad1463c633b6f08b80bd71b3fe204a59f9ead0
yekingyan <529616@gmail.com>
2019-05-21 17:13:01 +0800

    分类

--- 运维/gitlab-ci.mm
+++ 运维/gitlab-ci.mm
@@ -3,7 +3,7 @@
 <node TEXT="CI&#xff0f;CD" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1556510208185" STYLE="bubble">
 <font NAME="Noto Sans S Chinese Light" SIZE="14"/>
 <hook NAME="MapStyle" zoom="1.077">
-    <properties fit_to_viewport="false" show_icon_for_attributes="true" show_note_icons="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff"/>
+    <properties show_icon_for_attributes="true" fit_to_viewport="false" show_note_icons="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff"/>
 
 <map_styles>
 <stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
@@ -133,8 +133,26 @@
 </stylenode>
 </map_styles>
 </hook>
-<hook NAME="AutomaticEdgeColor" COUNTER="101" RULE="ON_BRANCH_CREATION"/>
+<hook NAME="AutomaticEdgeColor" COUNTER="106" RULE="ON_BRANCH_CREATION"/>
 <hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<node TEXT="CI &#x6301;&#x7eed;&#x96c6;&#x6210;" POSITION="right" ID="ID_1365350858" CREATED="1558428094577" MODIFIED="1558428105312">
+<edge COLOR="#00ff00"/>
+<node TEXT=" &#x9891;&#x7e41;&#x5730;&#xff08;&#x4e00;&#x5929;&#x591a;&#x6b21;&#xff09;&#x5c06;&#x4ee3;&#x7801;&#x96c6;&#x6210;&#x5230;&#x4e3b;&#x5e72;" ID="ID_1799978439" CREATED="1558428110803" MODIFIED="1558428112113"/>
+<node TEXT="&#x4f18;&#x70b9;" ID="ID_1732445274" CREATED="1558428154969" MODIFIED="1558428156799">
+<node TEXT=" &#x5feb;&#x901f;&#x53d1;&#x73b0;&#x9519;&#x8bef;" ID="ID_780549482" CREATED="1558428163239" MODIFIED="1558428164415">
+<node TEXT=" &#x6bcf;&#x5b8c;&#x6210;&#x4e00;&#x70b9;&#x66f4;&#x65b0;&#xff0c;&#x5c31;&#x96c6;&#x6210;&#x5230;&#x4e3b;&#x5e72;&#xff0c;&#x53ef;&#x4ee5;&#x5feb;&#x901f;&#x53d1;&#x73b0;&#x9519;&#x8bef;" ID="ID_79258045" CREATED="1558428137877" MODIFIED="1558428139061"/>
+</node>
+<node TEXT=" &#x9632;&#x6b62;&#x5206;&#x652f;&#x5927;&#x5e45;&#x504f;&#x79bb;&#x4e3b;&#x5e72;" ID="ID_141451382" CREATED="1558428151214" MODIFIED="1558428152135"/>
+</node>
+<node TEXT=" &#x76ee;&#x7684;" ID="ID_704249226" CREATED="1558428178885" MODIFIED="1558428185768">
+<node TEXT=" &#x8ba9;&#x4ea7;&#x54c1;&#x53ef;&#x4ee5;&#x5feb;&#x901f;&#x8fed;&#x4ee3;&#xff0c;&#x540c;&#x65f6;&#x8fd8;&#x80fd;&#x4fdd;&#x6301;&#x9ad8;&#x8d28;&#x91cf;" ID="ID_1040044671" CREATED="1558428187074" MODIFIED="1558428188014"/>
+<node TEXT=" &#x4ee3;&#x7801;&#x96c6;&#x6210;&#x5230;&#x4e3b;&#x5e72;&#x4e4b;&#x524d;&#xff0c;&#x5fc5;&#x987b;&#x901a;&#x8fc7;&#x81ea;&#x52a8;&#x5316;&#x6d4b;&#x8bd5;" ID="ID_452419049" CREATED="1558428201234" MODIFIED="1558428202303">
+<node TEXT=" &#x5355;&#x5143;&#x6d4b;&#x8bd5;" ID="ID_6280597" CREATED="1558428274483" MODIFIED="1558428282367"/>
+<node TEXT=" &#x96c6;&#x6210;&#x6d4b;&#x8bd5;" ID="ID_1704914933" CREATED="1558428283810" MODIFIED="1558428292735"/>
+<node TEXT=" &#x7aef;&#x5bf9;&#x7aef;&#x6d4b;&#x8bd5;" ID="ID_1156092391" CREATED="1558428293626" MODIFIED="1558428294927"/>
+</node>
+</node>
+</node>
 <node TEXT="&#x6301;&#x7eed;&#x6784;&#x5efa;" POSITION="right" ID="ID_131544224" CREATED="1556510216093" MODIFIED="1556542939288">
 <edge COLOR="#7c007c"/>
 <node TEXT=" &#x4ee3;&#x7801;&#x63d0;&#x4ea4;&#x540e;&#x89e6;&#x53d1;&#x81ea;&#x52a8;&#x5316;" ID="ID_1746253822" CREATED="1556510265247" MODIFIED="1556510266602"/>
@@ -143,17 +161,24 @@
 <node TEXT=" &#x6784;&#x5efa;&#x955c;&#x50cf;" ID="ID_1536331528" CREATED="1556510282963" MODIFIED="1556510287151"/>
 <node TEXT=" &#x4e0a;&#x4f20;&#x955c;&#x50cf;" ID="ID_1008402971" CREATED="1556510287979" MODIFIED="1556510290603"/>
 </node>
-<node TEXT="CD &#x6301;&#x7eed;&#x53d1;&#x5e03;" POSITION="right" ID="ID_1001647354" CREATED="1556510220356" MODIFIED="1556510302325">
-<edge COLOR="#007c7c"/>
-<node TEXT=" &#x6301;&#x7eed;&#x53d1;&#x5e03;&#x5219;&#x6307;&#x5c06;&#x6784;&#x5efa;&#x597d;&#x7684;&#x7a0b;&#x5e8f;&#x53d1;&#x5e03;&#x5230;&#x5404;&#x79cd;&#x73af;&#x5883;" ID="ID_903180439" CREATED="1556510315823" MODIFIED="1556510317154"/>
-<node TEXT="DEV" ID="ID_92590015" CREATED="1556510317755" MODIFIED="1556510319639"/>
-<node TEXT="TEST" ID="ID_1689268603" CREATED="1556510319846" MODIFIED="1556510321525"/>
-<node TEXT="PR" ID="ID_75066017" CREATED="1556510322090" MODIFIED="1556510327859"/>
-<node TEXT="PROD" ID="ID_1190519616" CREATED="1556510328295" MODIFIED="1556510332295"/>
+<node TEXT="CD" POSITION="right" ID="ID_1920206607" CREATED="1558428899821" MODIFIED="1558428902403">
+<edge COLOR="#00007c"/>
+<node TEXT=" CD &#x6301;&#x7eed;&#x4ea4;&#x4ed8; Continuous delivery" ID="ID_720694991" CREATED="1558428617252" MODIFIED="1558428932631">
+<node TEXT="&#x9891;&#x7e41;&#x5730;&#x5c06;&#x8f6f;&#x4ef6;&#x7684;&#x65b0;&#x7248;&#x672c;&#xff0c;&#x4ea4;&#x4ed8;&#x7ed9;&#x8d28;&#x91cf;&#x56e2;&#x961f;&#x6216;&#x8005;&#x7528;&#x6237;&#xff0c;&#x4ee5;&#x4f9b;&#x8bc4;&#x5ba1;" ID="ID_1321060195" CREATED="1558428662778" MODIFIED="1558428670448"/>
+<node TEXT=" &#x4e0d;&#x7ba1;&#x600e;&#x4e48;&#x66f4;&#x65b0;&#xff0c;&#x8f6f;&#x4ef6;&#x662f;&#x968f;&#x65f6;&#x968f;&#x5730;&#x53ef;&#x4ee5;&#x4ea4;&#x4ed8;&#x7684;" ID="ID_570107466" CREATED="1558428660692" MODIFIED="1558428661774"/>
+<node TEXT=" &#x5982;&#x679c;&#x8bc4;&#x5ba1;&#x901a;&#x8fc7;&#xff0c;&#x4ee3;&#x7801;&#x5c31;&#x8fdb;&#x5165;&#x751f;&#x4ea7;&#x9636;&#x6bb5;" ID="ID_1871478757" CREATED="1558428677686" MODIFIED="1558428678847"/>
+</node>
+<node TEXT="CD &#x6301;&#x7eed;&#x90e8;&#x7f72; continuous deployment" ID="ID_1001647354" CREATED="1556510220356" MODIFIED="1558428929387">
+<node TEXT=" &#x6301;&#x7eed;&#x90e8;&#x7f72;&#x662f;&#x6301;&#x7eed;&#x4ea4;&#x4ed8;&#x7684;&#x4e0b;&#x4e00;&#x6b65;" ID="ID_523831683" CREATED="1558428863880" MODIFIED="1558428881040"/>
+<node TEXT=" &#x4ee3;&#x7801;&#x5728;&#x4efb;&#x4f55;&#x65f6;&#x523b;&#x90fd;&#x662f;&#x53ef;&#x90e8;&#x7f72;&#x7684;&#xff0c;&#x53ef;&#x4ee5;&#x8fdb;&#x5165;&#x751f;&#x4ea7;&#x9636;&#x6bb5;" ID="ID_655951943" CREATED="1558428888178" MODIFIED="1558428889453"/>
+</node>
+<node TEXT="" ID="ID_851639634" CREATED="1558428909625" MODIFIED="1558428913702">
+<hook URI="gitlab-ci_files/png_779169037466465296.png" SIZE="1.0" NAME="ExternalObject"/>
+</node>
 </node>
 <node TEXT=" &#x6d41;&#x7a0b;" POSITION="right" ID="ID_498711276" CREATED="1556510399127" MODIFIED="1556510400497">
 <edge COLOR="#ff0000"/>
-<node TEXT=" &#x5199;&#x597d;.gitlab-ci.yml&#x6587;&#x4ef6;&#xff0c;&#x5e76;&#x653e;&#x7f6e;&#x5230;&#x9879;&#x76ee;&#x6839;&#x76ee;&#x5f55;" ID="ID_381916335" CREATED="1556510437394" MODIFIED="1556510438586"/>
+<node TEXT="&#x5199;&#x597d;.gitlab-ci.yml&#x6587;&#x4ef6;&#xff0c;&#x5e76;&#x653e;&#x7f6e;&#x5230;&#x9879;&#x76ee;&#x6839;&#x76ee;&#x5f55;" ID="ID_381916335" CREATED="1556510437394" MODIFIED="1558429022546"/>
 <node TEXT="&#x914d;&#x7f6e;&#x597d;gitlab runner&#xff0e;" ID="ID_1682050829" CREATED="1556510439135" MODIFIED="1556510446818">
 <node TEXT="&#x5b89;&#x88c5;" ID="ID_906892731" CREATED="1556510697509" MODIFIED="1556510702220"/>
 <node TEXT="&#x914d;&#x7f6e;" ID="ID_254094298" CREATED="1556510702451" MODIFIED="1556510704321"/>
@@ -183,7 +208,6 @@
     <pre style="background-color: #2b2b2b; color: #a9b7c6; font-family: &#x5b8b;&#x4f53;; font-size: 10.5pt"><span style="color: #cc7832; font-weight: bold"><font color="#cc7832"><b>build_dev</b></font></span>:<br/>  <span style="color: #cc7832; font-weight: bold"><font color="#cc7832"><b>script</b></font></span>:<br/>    - echo &quot;Building op-web image&quot;</pre>
   </body>
 </html>
-
 </richcontent>
 </node>
 </node>
@@ -209,7 +233,6 @@
     <pre style="background-color: #2b2b2b; color: #a9b7c6; font-family: &#x5b8b;&#x4f53;; font-size: 10.5pt"><span style="color: #cc7832; font-weight: bold"><font color="#cc7832"><b>script</b></font></span>:<br/>  - echo &quot;Building op-web image&quot;<br/>  - docker build -t $HUB_DOMAIN/web:dev .</pre>
   </body>
 </html>
-
 </richcontent>
 </node>
 <node ID="ID_1598815114" CREATED="1556521497131" MODIFIED="1556538025298"><richcontent TYPE="NODE">
@@ -222,7 +245,6 @@
     <pre style="background-color: #2b2b2b; color: #a9b7c6; font-family: &#x5b8b;&#x4f53;; font-size: 10.5pt"><span style="color: #cc7832; font-weight: bold"><font color="#cc7832"><b>build</b></font></span>:<br/>  <span style="color: #cc7832; font-weight: bold"><font color="#cc7832"><b>stage</b></font></span>: build<br/>  <span style="color: #cc7832; font-weight: bold"><font color="#cc7832"><b>script</b></font></span>:<br/>    - |<br/>      if [ &quot;$CI_COMMIT_REF_NAME&quot; = &quot;$DEPLOY_BRANCH&quot; ]; then<br/>        echo &quot;build ran and conditional was true&quot;<br/>      fi<br/></pre>
   </body>
 </html>
-
 </richcontent>
 </node>
 </node>
@@ -235,7 +257,7 @@
 <node TEXT=" &#x53ef;&#x4ee5;&#x662f;&#x4e00;&#x4e2a;&#x6570;&#x7ec4;&#x5143;&#x7d20;&#x6216;&#x8005;&#x4e00;&#x4e2a;&#x591a;&#x884c;&#x6587;&#x672c;&#x5143;&#x7d20;" ID="ID_867405899" CREATED="1556518669193" MODIFIED="1556518670618"/>
 </node>
 <node TEXT=" stages" ID="ID_1452774662" CREATED="1556518708399" MODIFIED="1556518713195">
-<node TEXT="  stages&#x5b9a;&#x4e49;&#x7684;&#x5143;&#x7d20;&#x987a;&#x5e8f;&#x51b3;&#x5b9a;&#x4e86;&#x6784;&#x5efa;&#x7684;&#x6267;&#x884c;&#x987a;&#x5e8f;" ID="ID_1085254721" CREATED="1556519157297" MODIFIED="1556519158641"/>
+<node TEXT=" stages&#x5b9a;&#x4e49;&#x7684;&#x5143;&#x7d20;&#x987a;&#x5e8f;&#x51b3;&#x5b9a;&#x4e86;&#x6784;&#x5efa;&#x7684;&#x6267;&#x884c;&#x987a;&#x5e8f;" ID="ID_1085254721" CREATED="1556519157297" MODIFIED="1558429309375"/>
 <node TEXT=" stages&#x7684;&#x522b;&#x540d;&#x4e3a;types" ID="ID_1138260390" CREATED="1556519110877" MODIFIED="1556519121162"/>
 <node TEXT="&#x6d41;&#x7a0b;" ID="ID_1024299368" CREATED="1556518792117" MODIFIED="1556519168907">
 <node TEXT=" &#x540c;&#x6837;stage&#x7684;job&#x662f;&#x5e76;&#x884c;&#x6267;&#x884c;&#x7684;" ID="ID_1923131293" CREATED="1556518798609" MODIFIED="1556518799484"/>
@@ -243,8 +265,8 @@
 <node TEXT=" &#x5982;&#x679c;&#x914d;&#x7f6e;&#x6587;&#x4ef6;&#x4e2d;&#x6ca1;&#x6709;&#x5b9a;&#x4e49;stages&#xff0c;&#x90a3;&#x4e48;&#x9ed8;&#x8ba4;&#x60c5;&#x51b5;&#x4e0b;&#x7684;stages&#x5c5e;&#x6027;&#x4e3a;build&#x3001;test&#x548c;deploy" ID="ID_1354154219" CREATED="1556519081252" MODIFIED="1556519083892"/>
 <node TEXT=" &#x5982;&#x679c;&#x4e00;&#x4e2a;job&#x6ca1;&#x6709;&#x5b9a;&#x4e49;stage&#x5c5e;&#x6027;&#xff0c;&#x5219;&#x5b83;&#x7684;stage&#x5c5e;&#x6027;&#x9ed8;&#x8ba4;&#x4e3a;test" ID="ID_475395299" CREATED="1556519090063" MODIFIED="1556519091059"/>
 </node>
-<node TEXT="&#x4f8b;&#x5b50;" ID="ID_489350248" CREATED="1556519007494" MODIFIED="1556519050966">
-<node ID="ID_1867573037" CREATED="1556519018453" MODIFIED="1556519018453"><richcontent TYPE="NODE">
+<node TEXT="&#x4f8b;&#x5b50;" ID="ID_489350248" CREATED="1556519007494" MODIFIED="1558429333966" VGAP_QUANTITY="6.8999999999999995 pt">
+<node ID="ID_1867573037" CREATED="1556519018453" MODIFIED="1558429333965"><richcontent TYPE="NODE">
 
 <html>
   <head>
@@ -254,7 +276,6 @@
     <pre style="background-color: #2b2b2b; color: #a9b7c6; font-family: &#x5b8b;&#x4f53;; font-size: 10.5pt"><span style="color: #cc7832; font-weight: bold"><font color="#cc7832"><b>stages</b></font></span>:<br/>- build<br/>- deploy</pre>
   </body>
 </html>
-
 </richcontent>
 </node>
 <node ID="ID_1894578794" CREATED="1556519027962" MODIFIED="1556519027962"><richcontent TYPE="NODE">
@@ -267,7 +288,6 @@
     <pre style="background-color: #2b2b2b; color: #a9b7c6; font-family: &#x5b8b;&#x4f53;; font-size: 10.5pt"><span style="color: #cc7832; font-weight: bold"><font color="#cc7832"><b>build_dev</b></font></span>:<br/>  <span style="color: #cc7832; font-weight: bold"><font color="#cc7832"><b>stage</b></font></span>: build</pre>
   </body>
 </html>
-
 </richcontent>
 </node>
 <node ID="ID_237283905" CREATED="1556519037807" MODIFIED="1556519037807"><richcontent TYPE="NODE">
@@ -280,7 +300,6 @@
     <pre style="background-color: #2b2b2b; color: #a9b7c6; font-family: &#x5b8b;&#x4f53;; font-size: 10.5pt"><span style="color: #cc7832; font-weight: bold"><font color="#cc7832"><b>deploy_dev</b></font></span>:<br/>  <span style="color: #cc7832; font-weight: bold"><font color="#cc7832"><b>stage</b></font></span>: deploy</pre>
   </body>
 </html>
-
 </richcontent>
 </node>
 </node>
@@ -315,7 +334,6 @@
     <pre style="background-color: #2b2b2b; color: #a9b7c6; font-family: &#x5b8b;&#x4f53;; font-size: 10.5pt"><span style="color: #cc7832; font-weight: bold"><font color="#cc7832"><b>build_dev</b></font></span>:<br/>  <span style="color: #cc7832; font-weight: bold"><font color="#cc7832"><b>stage</b></font></span>: build<br/>  <span style="color: #cc7832; font-weight: bold"><font color="#cc7832"><b>script</b></font></span>:<br/>    - echo &quot;Building op-web image&quot;<br/>  <span style="color: #cc7832; font-weight: bold"><font color="#cc7832"><b>only</b></font></span>:<br/>    - dev</pre>
   </body>
 </html>
-
 </richcontent>
 </node>
 </node>
@@ -330,7 +348,6 @@
     <pre style="background-color: #2b2b2b; color: #a9b7c6; font-family: &#x5b8b;&#x4f53;; font-size: 10.5pt"><span style="color: #cc7832; font-weight: bold"><font color="#cc7832"><b>build_test</b></font></span>:<br/>  <span style="color: #cc7832; font-weight: bold"><font color="#cc7832"><b>stage</b></font></span>: build<br/>  <span style="color: #cc7832; font-weight: bold"><font color="#cc7832"><b>script</b></font></span>:<br/>    - echo &quot;Building op-web image&quot;<br/>  <span style="color: #cc7832; font-weight: bold"><font color="#cc7832"><b>only</b></font></span>:<br/>    - tags</pre>
   </body>
 </html>
-
 </richcontent>
 </node>
 </node>
