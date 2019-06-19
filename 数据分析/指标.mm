c78a9bf791564d1576b9620e9937c9d693920082
yekingyan <529616@gmail.com>
2019-06-19 20:15:18 +0800

    数据分析 指标

---
+++ 数据分析/指标.mm
@@ -0,0 +1,212 @@
+<map version="freeplane 1.7.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="&#x6307;&#x6807;" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1560929289232" STYLE="bubble">
+<font NAME="Noto Sans S Chinese Light" SIZE="14"/>
+<hook NAME="MapStyle" zoom="1.077">
+    <properties fit_to_viewport="false" show_icon_for_attributes="true" show_note_icons="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font NAME="Noto Sans S Chinese Light" SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble" VGAP_QUANTITY="6.8999999999999995 pt">
+<font NAME="Noto Sans S Chinese Light" SIZE="12"/>
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="combined" BORDER_WIDTH="3.0 px" MAX_WIDTH="10.1 cm" MIN_WIDTH="0.0 cm" VGAP_QUANTITY="6.8999999999999995 pt">
+<font NAME="Noto Sans S Chinese Light" SIZE="12" BOLD="false" ITALIC="false"/>
+<edge STYLE="sharp_bezier" WIDTH="7"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details" VGAP_QUANTITY="6.8999999999999995 pt">
+<font NAME="Noto Sans S Chinese Light" SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes" VGAP_QUANTITY="6.8999999999999995 pt">
+<font NAME="Noto Sans S Chinese Light" SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT" VGAP_QUANTITY="6.8999999999999995 pt">
+<font NAME="Noto Sans S Chinese Light" SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating" VGAP_QUANTITY="6.8999999999999995 pt">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+<font NAME="Noto Sans S Chinese Light" SIZE="12"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble" VGAP_QUANTITY="6.8999999999999995 pt">
+<font NAME="Noto Sans S Chinese Light" SIZE="12"/>
+<stylenode LOCALIZED_TEXT="styles.ok" VGAP_QUANTITY="6.8999999999999995 pt">
+<icon BUILTIN="button_ok"/>
+<font NAME="Noto Sans S Chinese Light" SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.needs_action" VGAP_QUANTITY="6.8999999999999995 pt">
+<icon BUILTIN="messagebox_warning"/>
+<font NAME="Noto Sans S Chinese Light" SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.floating_node" VGAP_QUANTITY="6.8999999999999995 pt">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+<font NAME="Noto Sans S Chinese Light" SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork" VGAP_QUANTITY="6.8999999999999995 pt">
+<font NAME="Noto Sans S Chinese Light" SIZE="12" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork" VGAP_QUANTITY="6.8999999999999995 pt">
+<font NAME="Noto Sans S Chinese Light" SIZE="12" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900" VGAP_QUANTITY="6.8999999999999995 pt">
+<font NAME="Noto Sans S Chinese Light" SIZE="12" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.connection" COLOR="#606060" STYLE="fork" VGAP_QUANTITY="6.8999999999999995 pt">
+<font NAME="Noto Sans S Chinese Light" SIZE="12" BOLD="false"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important" COLOR="#ff0000" VGAP_QUANTITY="6.8999999999999995 pt">
+<icon BUILTIN="yes"/>
+<font NAME="Noto Sans S Chinese Light" SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.question" VGAP_QUANTITY="6.8999999999999995 pt">
+<icon BUILTIN="help"/>
+<font NAME="Noto Sans S Chinese Light" SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.key" COLOR="#996600" VGAP_QUANTITY="6.8999999999999995 pt">
+<icon BUILTIN="password"/>
+<font NAME="Noto Sans S Chinese Light" SIZE="12" BOLD="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.idea" VGAP_QUANTITY="6.8999999999999995 pt">
+<icon BUILTIN="idea"/>
+<font NAME="Noto Sans S Chinese Light" SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.note" COLOR="#990000" VGAP_QUANTITY="6.8999999999999995 pt">
+<font NAME="Noto Sans S Chinese Light" SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.date" COLOR="#0033ff" VGAP_QUANTITY="6.8999999999999995 pt">
+<icon BUILTIN="calendar"/>
+<font NAME="Noto Sans S Chinese Light" SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.website" COLOR="#006633" VGAP_QUANTITY="6.8999999999999995 pt">
+<font NAME="Noto Sans S Chinese Light" SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.list" COLOR="#cc6600" VGAP_QUANTITY="6.8999999999999995 pt">
+<icon BUILTIN="list"/>
+<font NAME="Noto Sans S Chinese Light" SIZE="12" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.quotation" COLOR="#338800" STYLE="fork" VGAP_QUANTITY="6.8999999999999995 pt">
+<font NAME="Noto Sans S Chinese Light" SIZE="12" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.definition" COLOR="#666600" VGAP_QUANTITY="6.8999999999999995 pt">
+<font NAME="Noto Sans S Chinese Light" SIZE="12" BOLD="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.description" COLOR="#996600" VGAP_QUANTITY="6.8999999999999995 pt">
+<font NAME="Noto Sans S Chinese Light" SIZE="12" BOLD="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.pending" COLOR="#b3b95c" VGAP_QUANTITY="6.8999999999999995 pt">
+<font NAME="Noto Sans S Chinese Light" SIZE="12"/>
+</stylenode>
+<stylenode TEXT="&#x4ee3;&#x7801;" COLOR="#c0fdce" BACKGROUND_COLOR="#000000" VGAP_QUANTITY="6.8999999999999995 pt">
+<font NAME="Noto Sans S Chinese Light" SIZE="12"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble" VGAP_QUANTITY="6.8999999999999995 pt">
+<font NAME="Noto Sans S Chinese Light" SIZE="12"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" VGAP_QUANTITY="6.8999999999999995 pt">
+<font NAME="Noto Sans S Chinese Light" SIZE="12"/>
+<edge COLOR="#003333"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff" VGAP_QUANTITY="6.8999999999999995 pt">
+<font NAME="Noto Sans S Chinese Light" SIZE="12"/>
+<edge COLOR="#808080"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#003366" VGAP_QUANTITY="6.8999999999999995 pt">
+<font NAME="Noto Sans S Chinese Light" SIZE="12"/>
+<edge COLOR="#808080"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000" VGAP_QUANTITY="6.8999999999999995 pt">
+<font NAME="Noto Sans S Chinese Light" SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111" VGAP_QUANTITY="6.8999999999999995 pt">
+<font NAME="Noto Sans S Chinese Light" SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5" COLOR="#006666" VGAP_QUANTITY="6.8999999999999995 pt">
+<font NAME="Noto Sans S Chinese Light" SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6" VGAP_QUANTITY="6.8999999999999995 pt">
+<font SIZE="12"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="106" RULE="ON_BRANCH_CREATION"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<node TEXT="&#x540c;&#x6bd4;&#x3001;&#x73af;&#x6bd4;" POSITION="right" ID="ID_970078451" CREATED="1560930931765" MODIFIED="1560930939679">
+<edge COLOR="#00ff00"/>
+<node TEXT="&#x540c;&#x6bd4;" ID="ID_505083168" CREATED="1560929289408" MODIFIED="1560930942633">
+<node TEXT=" year-on-year" ID="ID_1230755527" CREATED="1560932002200" MODIFIED="1560932003667">
+<node TEXT=" &#x4f18;&#x52bf;&#x5728;&#x4e8e;&#x53bb;&#x9664;&#x4e86;&#x5b63;&#x8282;&#x6027;&#x7684;&#x5f71;&#x54cd;" ID="ID_1172395618" CREATED="1560932633132" MODIFIED="1560932634362"/>
+</node>
+<node TEXT=" 2014&#x5e74;7&#x6708;&#x4efd;&#x4e0e;2013&#x5e74;7&#x6708;&#x4efd;&#x76f8;&#x6bd4;" ID="ID_141097930" CREATED="1560932133495" MODIFIED="1560932134466"/>
+<node TEXT=" &#x73af;&#x6bd4;&#x589e;&#x957f;&#x7387;=&#xff08;&#x672c;&#x671f;&#x6570;&#xff0d;&#x4e0a;&#x671f;&#x6570;&#xff09;/&#x4e0a;&#x671f;&#x6570;&#xd7;100%" ID="ID_502476356" CREATED="1560932391719" MODIFIED="1560932393122"/>
+<node TEXT="&#x9002;&#x7528;" ID="ID_330780080" CREATED="1560932461679" MODIFIED="1560932464031">
+<node TEXT=" &#x53d7;&#x5b63;&#x8282;&#x5f71;&#x54cd;&#x8f83;&#x5927;&#xff0c;&#x4e14;&#x6210;&#x957f;&#x6027;&#x4e0d;&#x660e;&#x663e;&#x7684;&#x516c;&#x53f8;&#x7684;&#x6536;&#x5165;&#x3001;&#x9500;&#x552e;&#x8d39;&#x7528;&#x7b49;&#x6570;&#x636e;" ID="ID_645920423" CREATED="1560932474041" MODIFIED="1560932546410"/>
+<node TEXT=" &#x96f6;&#x552e;&#x3001;&#x6c7d;&#x8f66;" ID="ID_1161804490" CREATED="1560932517112" MODIFIED="1560932518329"/>
+</node>
+</node>
+<node TEXT="&#x73af;&#x6bd4;" ID="ID_79837492" CREATED="1560929291213" MODIFIED="1560930941274">
+<node TEXT=" month-on-month" ID="ID_208070618" CREATED="1560931994704" MODIFIED="1560931996666">
+<node TEXT=" &#x5b63;&#x8282;&#x6027;&#x5bf9;&#x6570;&#x636e;&#x7684;&#x5f71;&#x54cd;&#x8f83;&#x5c0f;" ID="ID_940560824" CREATED="1560932650765" MODIFIED="1560932652145"/>
+</node>
+<node TEXT=" 2014&#x5e74;7&#x6708;&#x4efd;&#x4e0e;2014&#x5e74;6&#x6708;&#x4efd;&#x76f8;&#x6bd4;&#x8f83;" ID="ID_1375871820" CREATED="1560932120942" MODIFIED="1560932122818"/>
+<node TEXT=" &#x540c;&#x6bd4;&#x589e;&#x957f;&#x7387;=&#xff08;&#x672c;&#x671f;&#x6570;&#xff0d;&#x540c;&#x671f;&#x6570;&#xff09;/&#x540c;&#x671f;&#x6570;&#xd7;100%" ID="ID_136801245" CREATED="1560932398182" MODIFIED="1560932399433"/>
+<node TEXT="&#x9002;&#x7528;" ID="ID_1498154341" CREATED="1560932465784" MODIFIED="1560932468289">
+<node TEXT=" &#x6210;&#x957f;&#x6027;&#x7279;&#x522b;&#x5f3a;&#x6216;&#x8005;&#x4e1a;&#x52a1;&#x53d7;&#x5b63;&#x8282;&#x5f71;&#x54cd;&#x5c0f;&#x7684;&#x516c;&#x53f8;&#x7684;&#x6536;&#x5165;&#x3001;&#x9500;&#x552e;&#x8d39;&#x7528;&#x7b49;&#x6570;&#x636e;" ID="ID_753875117" CREATED="1560932527978" MODIFIED="1560932542961"/>
+<node TEXT="&#x521d;&#x521b;&#x516c;&#x53f8;" ID="ID_1422301072" CREATED="1560932530123" MODIFIED="1560932562099"/>
+<node TEXT="&#x7edd;&#x5927;&#x591a;&#x6570;&#x8d39;&#x7528;&#x6570;&#x636e;&#xff0c;&#x4f8b;&#x5982;&#x6298;&#x65e7;&#x3001;&#x623f;&#x79df;&#x3001;&#x4fdd;&#x9669;&#x8d39;&#x7b49;" ID="ID_1335774399" CREATED="1560932562289" MODIFIED="1560932573914"/>
+</node>
+</node>
+<node TEXT="" ID="ID_512195276" CREATED="1560930996434" MODIFIED="1560931000490">
+<hook URI="&#x6307;&#x6807;_files/png_3473618757853625161.png" SIZE="0.8333333" NAME="ExternalObject"/>
+</node>
+</node>
+<node TEXT="UV&#x4e0e;IP" POSITION="right" ID="ID_286058874" CREATED="1560933922713" MODIFIED="1560933930037">
+<edge COLOR="#7c0000"/>
+<node TEXT="UV" ID="ID_1902050795" CREATED="1560929295900" MODIFIED="1560933933161">
+<node TEXT=" Unique visitor" ID="ID_1823842567" CREATED="1560933773937" MODIFIED="1560933775339"/>
+<node TEXT="&#x7edf;&#x8ba1;&#x4f9d;&#x636e;" ID="ID_673688747" CREATED="1560934298372" MODIFIED="1560934301970">
+<node TEXT="&#x4ee5;&#x5e10;&#x53f7;&#x4f5c;&#x4e3a;&#x7edf;&#x8ba1;&#x4f9d;&#x636e;" ID="ID_681167883" CREATED="1560933896059" MODIFIED="1560933909309"/>
+<node TEXT="google&#x7edf;&#x8ba1;&#x5de5;&#x5177;&#xff0c;&#x4ee5;&#x6d4f;&#x89c8;&#x5668;cookie&#x4e3a;&#x4f9d;&#x636e;&#x7684;" ID="ID_819877348" CREATED="1560934304399" MODIFIED="1560934352461"/>
+</node>
+<node TEXT=" &#x4e00;&#x5929;&#x5185;&#x540c;&#x4e2a;&#x8bbf;&#x5ba2;&#x591a;&#x6b21;&#x8bbf;&#x95ee;&#x4ec5;&#x8ba1;&#x7b97;&#x4e00;&#x4e2a;UV" ID="ID_1883158212" CREATED="1560933811858" MODIFIED="1560933813100"/>
+</node>
+<node TEXT="IP" ID="ID_1818741192" CREATED="1560933819822" MODIFIED="1560933931796">
+<node TEXT=" Internet Protocol" ID="ID_797325682" CREATED="1560933835746" MODIFIED="1560933836709"/>
+<node TEXT="&#x72ec;&#x7acb;IP&#x662f;&#x6307;&#x8bbf;&#x95ee;&#x8fc7;&#x67d0;&#x7ad9;&#x70b9;&#x7684;IP&#x603b;&#x6570;&#xff0c;&#x4ee5;&#x7528;&#x6237;&#x7684;IP&#x5730;&#x5740;&#x4f5c;&#x4e3a;&#x7edf;&#x8ba1;&#x4f9d;&#x636e;" ID="ID_1060016099" CREATED="1560933851447" MODIFIED="1560933855532"/>
+<node TEXT="  &#x4e00;&#x5929;&#x5185;&#x540c;&#x4e00;&#x4e2a;IP&#x5730;&#x5740;&#x591a;&#x6b21;&#x8bbf;&#x95ee;&#x4ec5;&#x8ba1;&#x7b97;&#x4e00;&#x4e2a;IP" ID="ID_366292103" CREATED="1560933858181" MODIFIED="1560933878471"/>
+</node>
+<node TEXT="UV&#x4e0e;IP&#x533a;&#x522b;" ID="ID_1129956206" CREATED="1560933917988" MODIFIED="1560933938676">
+<node TEXT="&#x4e00;&#x5929;&#x4e2d;&#xff0c;&#x4e24;&#x4e2a;&#x8d26;&#x53f7;&#x540c;&#x4e00;&#x53f0;&#x7535;&#x8111;" ID="ID_1700127971" CREATED="1560933965615" MODIFIED="1560933997122"/>
+<node TEXT="&#x540c;&#x4e00;ip&#x5730;&#x5740;&#xff0c;IP+1" ID="ID_360285379" CREATED="1560933988041" MODIFIED="1560934018981"/>
+<node TEXT="&#x4e24;&#x4e2a;&#x8d26;&#x53f7;&#xff0c;UV+2" ID="ID_1232864437" CREATED="1560934019260" MODIFIED="1560934030945"/>
+</node>
+</node>
+<node TEXT="PV &#x4e0e; VV" POSITION="right" ID="ID_1376134983" CREATED="1560934148082" MODIFIED="1560934153336">
+<edge COLOR="#00007c"/>
+<node TEXT="PV" ID="ID_1194295898" CREATED="1560929292968" MODIFIED="1560934156345">
+<node TEXT=" Page View &#x5373;&#x9875;&#x9762;&#x6d4f;&#x89c8;&#x91cf;&#x6216;&#x70b9;&#x51fb;&#x91cf;" ID="ID_1144302228" CREATED="1560934063924" MODIFIED="1560934074499"/>
+<node TEXT=" &#x7528;&#x6237;&#x6bcf;1&#x6b21;&#x5bf9;&#x7f51;&#x7ad9;&#x4e2d;&#x7684;&#x6bcf;&#x4e2a;&#x7f51;&#x9875;&#x8bbf;&#x95ee;&#x5747;&#x88ab;&#x8bb0;&#x5f55;1&#x4e2a;PV" ID="ID_1340339911" CREATED="1560934091068" MODIFIED="1560934092268"/>
+<node TEXT=" &#x7528;&#x6237;&#x5bf9;&#x540c;&#x4e00;&#x9875;&#x9762;&#x7684;&#x591a;&#x6b21;&#x8bbf;&#x95ee;&#xff0c;&#x8bbf;&#x95ee;&#x91cf;&#x7d2f;&#x8ba1;" ID="ID_1625762284" CREATED="1560934099900" MODIFIED="1560934101067"/>
+<node TEXT=" &#x7528;&#x4ee5;&#x8861;&#x91cf;&#x7f51;&#x7ad9;&#x7528;&#x6237;&#x8bbf;&#x95ee;&#x7684;&#x7f51;&#x9875;&#x6570;&#x91cf;" ID="ID_1799455369" CREATED="1560934107326" MODIFIED="1560934108316"/>
+</node>
+<node TEXT="VV" ID="ID_520722315" CREATED="1560929297006" MODIFIED="1560934154804">
+<node TEXT=" Visit View" ID="ID_1070301938" CREATED="1560934112699" MODIFIED="1560934113612"/>
+<node TEXT=" &#x7528;&#x4ee5;&#x7edf;&#x8ba1;&#x6240;&#x6709;&#x8bbf;&#x5ba2;1&#x5929;&#x5185;&#x8bbf;&#x95ee;&#x7f51;&#x7ad9;&#x7684;&#x6b21;&#x6570;" ID="ID_558330669" CREATED="1560934125663" MODIFIED="1560934126642"/>
+<node TEXT="&#x5f53;&#x8bbf;&#x5ba2;&#x5b8c;&#x6210;&#x6240;&#x6709;&#x6d4f;&#x89c8;&#x5e76;&#x6700;&#x7ec8;&#x5173;&#x6389;&#x8be5;&#x7f51;&#x7ad9;&#x7684;&#x6240;&#x6709;&#x9875;&#x9762;&#x65f6;&#x4fbf;&#x5b8c;&#x6210;&#x4e86;&#x4e00;&#x6b21;&#x8bbf;&#x95ee;" ID="ID_332339746" CREATED="1560934130419" MODIFIED="1560934138523"/>
+<node TEXT=" &#x540c;&#x4e00;&#x8bbf;&#x5ba2;1&#x5929;&#x5185;&#x53ef;&#x80fd;&#x6709;&#x591a;&#x6b21;&#x8bbf;&#x95ee;&#x884c;&#x4e3a;&#xff0c;&#x8bbf;&#x95ee;&#x6b21;&#x6570;&#x7d2f;&#x8ba1;" ID="ID_525970220" CREATED="1560934143197" MODIFIED="1560934144203"/>
+</node>
+<node TEXT="PV&#x4e0e;VV&#x533a;&#x522b;" ID="ID_978599984" CREATED="1560934157712" MODIFIED="1560934162938">
+<node TEXT="&#x4f8b;" ID="ID_1216426914" CREATED="1560934178918" MODIFIED="1560934182134">
+<node TEXT="&#x4f60;&#x4eca;&#x5929;10&#x70b9;&#x949f;&#x6253;&#x5f00;&#x4e86;&#x767e;&#x5ea6;&#xff0c;&#x8bbf;&#x95ee;&#x4e86;&#x5b83;&#x7684;&#x4e09;&#x4e2a;&#x9875;&#x9762;" ID="ID_758024407" CREATED="1560934182461" MODIFIED="1560934231275"/>
+<node TEXT="11&#x70b9;&#x949f;&#x53c8;&#x6253;&#x5f00;&#x4e86;&#x767e;&#x5ea6;&#xff0c;&#x8bbf;&#x95ee;&#x4e86;&#x5b83;&#x7684;&#x4e24;&#x4e2a;&#x9875;&#x9762;" ID="ID_1898867547" CREATED="1560934190327" MODIFIED="1560934229455"/>
+<node TEXT="PV&#x6570;+5&#xff0c;VV&#x6570;+2" ID="ID_189326646" CREATED="1560934198285" MODIFIED="1560934208250"/>
+</node>
+<node TEXT=" PV&#x662f;&#x6307;&#x9875;&#x9762;&#x7684;&#x6d4f;&#x89c8;&#x6b21;&#x6570;&#xff0c;VV&#x662f;&#x6307;&#x4f60;&#x8bbf;&#x95ee;&#x7f51;&#x7ad9;&#x7684;&#x6b21;&#x6570;" ID="ID_998068110" CREATED="1560934216028" MODIFIED="1560934217227"/>
+</node>
+</node>
+</node>
+</map>
