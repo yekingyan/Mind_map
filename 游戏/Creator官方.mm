2de196d3a5f8c8cddcbcc84e401c39a5d9e7f356
yekingyan <529616@gmail.com>
2020-04-15 01:35:49 +0800

    cocos creator

---
+++ 游戏/Creator官方.mm
@@ -0,0 +1,239 @@
+<map version="freeplane 1.8.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Creator官方" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1586875243717" STYLE="bubble">
+<font NAME="Noto Sans S Chinese Light" SIZE="14"/>
+<hook NAME="MapStyle" zoom="1.077">
+    <properties fit_to_viewport="false" show_icon_for_attributes="true" show_note_icons="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font NAME="Noto Sans S Chinese Light" SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble" VGAP_QUANTITY="6.8999999999999995 pt">
+<font NAME="Noto Sans S Chinese Light" SIZE="12"/>
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="combined" BORDER_WIDTH="3.0 px" MAX_WIDTH="10.1 cm" MIN_WIDTH="0.0 cm" VGAP_QUANTITY="6.8999999999999995 pt">
+<font NAME="Noto Sans S Chinese Light" SIZE="12" BOLD="false" ITALIC="false"/>
+<edge STYLE="sharp_bezier" WIDTH="7"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details" VGAP_QUANTITY="6.8999999999999995 pt">
+<font NAME="Noto Sans S Chinese Light" SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes" VGAP_QUANTITY="6.8999999999999995 pt">
+<font NAME="Noto Sans S Chinese Light" SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT" VGAP_QUANTITY="6.8999999999999995 pt">
+<font NAME="Noto Sans S Chinese Light" SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating" VGAP_QUANTITY="6.8999999999999995 pt">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+<font NAME="Noto Sans S Chinese Light" SIZE="12"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble" VGAP_QUANTITY="6.8999999999999995 pt">
+<font NAME="Noto Sans S Chinese Light" SIZE="12"/>
+<stylenode LOCALIZED_TEXT="styles.ok" VGAP_QUANTITY="6.8999999999999995 pt">
+<icon BUILTIN="button_ok"/>
+<font NAME="Noto Sans S Chinese Light" SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.needs_action" VGAP_QUANTITY="6.8999999999999995 pt">
+<icon BUILTIN="messagebox_warning"/>
+<font NAME="Noto Sans S Chinese Light" SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.floating_node" VGAP_QUANTITY="6.8999999999999995 pt">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+<font NAME="Noto Sans S Chinese Light" SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork" VGAP_QUANTITY="6.8999999999999995 pt">
+<font NAME="Noto Sans S Chinese Light" SIZE="12" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork" VGAP_QUANTITY="6.8999999999999995 pt">
+<font NAME="Noto Sans S Chinese Light" SIZE="12" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900" VGAP_QUANTITY="6.8999999999999995 pt">
+<font NAME="Noto Sans S Chinese Light" SIZE="12" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.connection" COLOR="#606060" STYLE="fork" VGAP_QUANTITY="6.8999999999999995 pt">
+<font NAME="Noto Sans S Chinese Light" SIZE="12" BOLD="false"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important" COLOR="#ff0000" VGAP_QUANTITY="6.8999999999999995 pt">
+<icon BUILTIN="yes"/>
+<font NAME="Noto Sans S Chinese Light" SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.question" VGAP_QUANTITY="6.8999999999999995 pt">
+<icon BUILTIN="help"/>
+<font NAME="Noto Sans S Chinese Light" SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.key" COLOR="#996600" VGAP_QUANTITY="6.8999999999999995 pt">
+<icon BUILTIN="password"/>
+<font NAME="Noto Sans S Chinese Light" SIZE="12" BOLD="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.idea" VGAP_QUANTITY="6.8999999999999995 pt">
+<icon BUILTIN="idea"/>
+<font NAME="Noto Sans S Chinese Light" SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.note" COLOR="#990000" VGAP_QUANTITY="6.8999999999999995 pt">
+<font NAME="Noto Sans S Chinese Light" SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.date" COLOR="#0033ff" VGAP_QUANTITY="6.8999999999999995 pt">
+<icon BUILTIN="calendar"/>
+<font NAME="Noto Sans S Chinese Light" SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.website" COLOR="#006633" VGAP_QUANTITY="6.8999999999999995 pt">
+<font NAME="Noto Sans S Chinese Light" SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.list" COLOR="#cc6600" VGAP_QUANTITY="6.8999999999999995 pt">
+<icon BUILTIN="list"/>
+<font NAME="Noto Sans S Chinese Light" SIZE="12" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.quotation" COLOR="#338800" STYLE="fork" VGAP_QUANTITY="6.8999999999999995 pt">
+<font NAME="Noto Sans S Chinese Light" SIZE="12" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.definition" COLOR="#666600" VGAP_QUANTITY="6.8999999999999995 pt">
+<font NAME="Noto Sans S Chinese Light" SIZE="12" BOLD="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.description" COLOR="#996600" VGAP_QUANTITY="6.8999999999999995 pt">
+<font NAME="Noto Sans S Chinese Light" SIZE="12" BOLD="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.pending" COLOR="#b3b95c" VGAP_QUANTITY="6.8999999999999995 pt">
+<font NAME="Noto Sans S Chinese Light" SIZE="12"/>
+</stylenode>
+<stylenode TEXT="代码" COLOR="#c0fdce" BACKGROUND_COLOR="#000000" VGAP_QUANTITY="6.8999999999999995 pt">
+<font NAME="Noto Sans S Chinese Light" SIZE="12"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble" VGAP_QUANTITY="6.8999999999999995 pt">
+<font NAME="Noto Sans S Chinese Light" SIZE="12"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" VGAP_QUANTITY="6.8999999999999995 pt">
+<font NAME="Noto Sans S Chinese Light" SIZE="12"/>
+<edge COLOR="#003333"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff" VGAP_QUANTITY="6.8999999999999995 pt">
+<font NAME="Noto Sans S Chinese Light" SIZE="12"/>
+<edge COLOR="#808080"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#003366" VGAP_QUANTITY="6.8999999999999995 pt">
+<font NAME="Noto Sans S Chinese Light" SIZE="12"/>
+<edge COLOR="#808080"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000" VGAP_QUANTITY="6.8999999999999995 pt">
+<font NAME="Noto Sans S Chinese Light" SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111" VGAP_QUANTITY="6.8999999999999995 pt">
+<font NAME="Noto Sans S Chinese Light" SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5" COLOR="#006666" VGAP_QUANTITY="6.8999999999999995 pt">
+<font NAME="Noto Sans S Chinese Light" SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6" VGAP_QUANTITY="6.8999999999999995 pt">
+<font SIZE="12"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="100" RULE="ON_BRANCH_CREATION"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<node TEXT="组件化开发" POSITION="right" ID="ID_1441408559" CREATED="1586876119453" MODIFIED="1586876256160">
+<edge COLOR="#7c007c"/>
+<node TEXT="对比cocos-2dx" ID="ID_753506252" CREATED="1586877143674" MODIFIED="1586877150308">
+<node TEXT=" cocos-2dx以继承的形式拓展功能,给节点添加功能，必需新建一个类继承Node节点类，需要多少个节点，就要建多少个类" ID="ID_1638170320" CREATED="1586876234531" MODIFIED="1586877160842"/>
+<node TEXT="coreator写好的脚本以组件化的形式添加到节点上，组合组件提供无限可能" ID="ID_110419028" CREATED="1586876497716" MODIFIED="1586877138529" VGAP_QUANTITY="6.8999999999999995 pt"/>
+</node>
+<node TEXT="脚本" ID="ID_405783583" CREATED="1586877165664" MODIFIED="1586877587765">
+<node TEXT="extends 声明父类" ID="ID_37405036" CREATED="1586878322460" MODIFIED="1586878439069">
+<node TEXT=" extends: cc.Component" ID="ID_9033559" CREATED="1586878445425" MODIFIED="1586878446497"/>
+<node TEXT=" 声明这个 Class 的父类是 cc.Component" ID="ID_626164098" CREATED="1586878453392" MODIFIED="1586878458353"/>
+<node TEXT=" cc.Component 是 Cocos Creator 的内置类型" ID="ID_898342935" CREATED="1586878459019" MODIFIED="1586878461084"/>
+</node>
+<node TEXT="properties 定义属性" ID="ID_1728522856" CREATED="1586877588006" MODIFIED="1586878310943">
+<node ID="ID_683891601" CREATED="1586877699190" MODIFIED="1586878239239"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <div style="color: rgb(212, 212, 212); background-color: rgb(30, 30, 30); font-family: Consolas, Courier New, monospace; line-height: 19px; white-space: pre">
+      <div>
+        <font color="#9cdcfe">properties:</font>&nbsp;{
+      </div>
+      <div>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<font color="#9cdcfe">label:</font>&nbsp;{
+      </div>
+      <div>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<font color="#9cdcfe">default:</font>&nbsp;<font color="#569cd6">null</font>,
+      </div>
+      <div>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<font color="#9cdcfe">type:</font>&nbsp;<font color="#9cdcfe">cc</font>.<font color="#9cdcfe">Label</font>
+      </div>
+      <div>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;},
+      </div>
+      <div>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<font color="#9cdcfe">text:</font>&nbsp;<font color="#ce9178">'Hello,&nbsp;Worldaaa'</font>
+      </div>
+      <div>
+        &nbsp;&nbsp;&nbsp;&nbsp;},
+      </div>
+    </div>
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node TEXT="引用类型" ID="ID_986488855" CREATED="1586877672151" MODIFIED="1586877677428">
+<node TEXT="cc节点" ID="ID_176890002" CREATED="1586878255029" MODIFIED="1586878259252"/>
+<node TEXT="用type指定" ID="ID_849775690" CREATED="1586878268979" MODIFIED="1586878280260"/>
+</node>
+<node TEXT="非引用类型" ID="ID_600923752" CREATED="1586877678085" MODIFIED="1586877686920">
+<node TEXT="字符串或数字等" ID="ID_510516056" CREATED="1586877687289" MODIFIED="1586877698999"/>
+</node>
+</node>
+</node>
+<node TEXT="组件化与数据驱动的优势" ID="ID_1492616645" CREATED="1586878724642" MODIFIED="1586878736370">
+<node TEXT=" 脚本拖拽到节点后，属性值不以脚本为准，以编辑器显示的值为准。" ID="ID_1422716356" CREATED="1586878742108" MODIFIED="1586878743100"/>
+<node TEXT="脚本程序只要声明了这个属性，后续不用修改代码即可完成属性值的修改，修改后的属性保存在场景的资源文件中" ID="ID_1335678895" CREATED="1586878779404" MODIFIED="1586878877832"/>
+<node TEXT="分离了数据与功能，给予美术策划直接生产与管理内容的能力" ID="ID_1514000061" CREATED="1586878525153" MODIFIED="1586878953352"/>
+</node>
+</node>
+<node TEXT="UI系统" POSITION="right" ID="ID_1128233897" CREATED="1586884232141" MODIFIED="1586884240105">
+<edge COLOR="#ff0000"/>
+<node TEXT="sprite精灵" ID="ID_413022484" CREATED="1586885636120" MODIFIED="1586885642407">
+<node TEXT="添加一张图片" ID="ID_411055904" CREATED="1586885642641" MODIFIED="1586885652005"/>
+</node>
+<node TEXT="label" ID="ID_1269073810" CREATED="1586885653623" MODIFIED="1586885657821">
+<node TEXT="文字" ID="ID_1926227855" CREATED="1586885658180" MODIFIED="1586885662785"/>
+</node>
+<node TEXT="多分辨率自适应控件" ID="ID_224264283" CREATED="1586884762803" MODIFIED="1586884786129">
+<node TEXT="Widget 对齐挂件" ID="ID_1000863674" CREATED="1586884804280" MODIFIED="1586884834719">
+<node TEXT="实现多分辨离线地图自适应的排版的基础" ID="ID_416207620" CREATED="1586884814284" MODIFIED="1586884857116"/>
+<node TEXT="子节点相对父节点对齐" ID="ID_646779583" CREATED="1586885074922" MODIFIED="1586885092726"/>
+<node TEXT="设置与父节点的对齐方式与距离" ID="ID_361710039" CREATED="1586884868838" MODIFIED="1586884902937"/>
+</node>
+<node TEXT="Layout 布局" ID="ID_629006779" CREATED="1586885098588" MODIFIED="1586885104931">
+<node TEXT="子节点排列种类" ID="ID_524395320" CREATED="1586885128738" MODIFIED="1586885203802">
+<node TEXT="水平" ID="ID_892425374" CREATED="1586885131856" MODIFIED="1586885134817"/>
+<node TEXT="垂直" ID="ID_1089412091" CREATED="1586885135000" MODIFIED="1586885148749"/>
+</node>
+<node TEXT=" Resize" ID="ID_571236620" CREATED="1586885150796" MODIFIED="1586885260119">
+<node TEXT="CHILDREN自适应改变子节点大小" ID="ID_701199287" CREATED="1586885260610" MODIFIED="1586885308302"/>
+<node TEXT="CONTAINER自适应改变Layout大小" ID="ID_884481575" CREATED="1586885281494" MODIFIED="1586885318505"/>
+</node>
+<node TEXT="Padding 子节点与父节点边距" ID="ID_1256377831" CREATED="1586885332189" MODIFIED="1586885406259"/>
+<node TEXT="Spacing X  子节点之间的边距" ID="ID_297659294" CREATED="1586885380286" MODIFIED="1586885402023"/>
+</node>
+</node>
+<node TEXT="ScrollView" ID="ID_195590923" CREATED="1586885679220" MODIFIED="1586885687622"/>
+<node TEXT="ScrollView + Layout" ID="ID_506158588" CREATED="1586885687841" MODIFIED="1586885699341"/>
+<node TEXT="Prefab" ID="ID_936748553" CREATED="1586885699587" MODIFIED="1586885703524"/>
+</node>
+<node TEXT="api" POSITION="right" ID="ID_644754138" CREATED="1586877543854" MODIFIED="1586877545687">
+<edge COLOR="#7c7c00"/>
+<node TEXT="onload()" ID="ID_938538279" CREATED="1586877546059" MODIFIED="1586878493176"/>
+<node TEXT="onStart()" ID="ID_1388512664" CREATED="1586878475841" MODIFIED="1586878496616"/>
+<node TEXT="update(dt)" ID="ID_1089072238" CREATED="1586877550687" MODIFIED="1586878500808"/>
+</node>
+</node>
+</map>
